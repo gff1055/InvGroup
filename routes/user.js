7926195d6624a85617a9836f8dedfe8866a52538
gff1055 <gff105@gmail.com>
2025-01-02 19:05:13 -0300

    adicao de usuarios

--- routes/user.js
+++ routes/user.js
@@ -4,20 +4,27 @@ const userService = require("../services/UserService")
 
 router.get('/', function(req, res){
     
+    // funcao que retorna todos os dados
     userService.allData().then((users) => {
-        /*console.log("USERS EM GET");console.log(users);*/
-        //es.send("pagina principal GET de users")
     res.render("user/index", {users: users})
     });    
 })
 
+
+
+
 router.post('/', function(req, res){
+
+    // funcao para adicionar usuario é chamada
     let varFunc = userService.store(req, res)
         .then((answer) => {
-            console.log("dentro do console" + answer.success);
+
+            // Se houve falha na adição, a pagina é renderizada novamente indicando o erro
             if(answer.success != true){
                 res.render("user/index", {feedback:answer})
             }
+
+            // Se houver sucesso, a mensagem é exibida
             else{
                 console.log("passou aqui " + answer.success)
                 req.flash("success_msg", "Usuario cadastrado!")
@@ -25,6 +32,33 @@ router.post('/', function(req, res){
             }
         });
 })
+
+
+
+/** rota para excluir um usuario */
+router.post('/delete', function(req, res){
+
+    // chamando funcao para excluir o usuario
+    userService.destroy(req, res)
+    .then((answer)=>{
+
+        // Se houve falha na exclusao, a pagina é renderizada novamente indicando o erro
+        if(answer.success != true){
+            res.render("user/index", {feedback:answer})
+        }
+
+        // Se houver sucesso, a mensagem é exibida
+        else{
+            req.flash("success_msg", "Usuario excluido")
+            res.redirect("/user")
+        }
+    });
+})
+
+
+/*router.delete('/:id', function(req, res){
+    res.send("pagina DELETE do usuario" + req.params.id)
+})*/
 
 router.get('/create', function(req, res){
     res.send("pagina GET de /create")
@@ -40,10 +74,6 @@ router.get('/:id', function(req, res){
 
 router.put('/:id', function(req, res){
     res.send("pagina PUT do usuario" + req.params.id)
-})
-
-router.delete('/:id', function(req, res){
-    res.send("pagina DELETE do usuario" + req.params.id)
 })
 
 router.get('/:id/edit', function(req, res){
--- services/UserService.js
+++ services/UserService.js
@@ -1,8 +1,9 @@
-const repository = require('../models/Users')
+const repository = require('../models/Users') // model da tabela de usuarios
 
 
 const userService = {
 
+    /** Formata o CPF para cisualizacao */
     formatCPF: function(cpf){
         
         //retira os caracteres indesejados...
@@ -12,6 +13,9 @@ const userService = {
         return cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4");
     },
 
+
+
+    /** Formata o telefone para visualizaca */
     formatPhone: function(phone){
         
         //retira os caracteres indesejados...
@@ -21,6 +25,9 @@ const userService = {
         return phone.replace(/(\d{2})(\d{4})(\d{4})/, "($1)$2-$3");
     },
 
+    
+    /** Armazena o usuario no banco */
+    /** Retorna o resultado da execucao */
     store: async function(req, res){
 
         var feedback = {
@@ -30,7 +37,8 @@ const userService = {
         }
 
         
-
+        // Verifica se o nome do usuario é valido
+        
         if(!req.body.name || typeof req.body.name == undefined || req.body.name == null)
             feedback.erros.push({texto: "Nome invalido"})
         if(!req.body.phone || typeof req.body.phone == undefined || req.body.phone == null)
@@ -40,17 +48,19 @@ const userService = {
         if(!req.body.password || typeof req.body.password == undefined || req.body.password == null)
             feedback.erros.push({texto: "Password invalido"})
 
+        /**Se nao houver erros o usuario é inserido */
         if(feedback.erros.length == 0){
-            await repository.create({
+
+            await repository.create({           // Adiciona o usuario
                 cpf: req.body.cpf,
                 name: req.body.name,
                 phone: req.body.phone,
                 email: req.body.email,
                 password: req.body.password,
-            }).then(function(){
+            }).then(function(){                     // Em caso de sucesso na insercao a mensagem é exibida
                 feedback.success = true;
                 console.log("feedback.success = true" + feedback.success);
-            }).catch(function(){
+            }).catch(function(){                        // Em caso de erro uma mensagem é exibida
                 req.flash("error_msg", "Houve um erro ao salvar o usuario, tente novamente")
                 console.log("Erro ao salvar o usuario")
                 feedback.exception = true;
@@ -63,15 +73,16 @@ const userService = {
         return feedback;
     },
 
-
+    /** Retorna todos os dados de usuarios */
     allData: async function(){
         let users;
-        await repository.findAll().then(function(answer){
+        await repository.findAll().then(function(answer){               // Todos os usuarios sao buscados
             users = answer;
+
+            // Os CPF e telefones sao formatados para exibicao
             for(i = 0; i<users.length; i++){
                 users[i].cpf = userService.formatCPF(users[i].cpf);
                 users[i].phone = userService.formatPhone(users[i].phone);
-                console.log(userService.formatCPF(users[i].cpf));
             }
         })
 
@@ -84,8 +95,28 @@ const userService = {
     },
     
 
-    delet: function(){
-    
+    destroy: async function(req, res){
+
+        // objeto para enviar feedback
+        let feedback = {
+            erros:[],
+            success: false,
+            exception: false
+        }
+
+        // objeto sendo excluido....
+        await repository.destroy({where:{'id': req.body.id}})
+
+        // havendo sucesso ou falha na exclusao, o objeto de feedback recebe o resultado
+        .then(function(){
+            feedback.success = true;
+        })
+        .catch(function(erro){
+            feedback.exception = true;
+        })
+      
+       return feedback;
+
     },
 
 };
