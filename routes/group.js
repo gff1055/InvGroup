6dc272d50f68f14a12c5c9cdadbfd4f34f82e0e1
gff1055 <gff105@gmail.com>
2025-02-05 15:07:08 -0300

    criacao da view index de grupos

--- routes/group.js
+++ routes/group.js
@@ -2,7 +2,7 @@ const express = require("express")
 const router = express.Router()
 
 router.get('/', function(req, res){
-    res.send("pagina principal GET de grupo")
+    res.render('groups/index');
 })
 
 router.post('/', function(req, res){
